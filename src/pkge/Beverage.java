30c70750b161a8eea36fa4afe9add941d0f6b068
Glenn <casetoolbca201@gmail.com>
2024-02-10 09:01:18 +0530

    Code1

--- src/pkge/Beverage.java
+++ src/pkge/Beverage.java
@@ -1,5 +1,13 @@
 package pkge;
 
 public class Beverage {
-
+	String type;
+	Beverage(String type){
+		this.type=type;
+	}
+	public static void main(String[]args)
+	{
+		Beverage tea= new tea("hot");
+		System.out.print("tea"+tea.type);
+	}
 }
